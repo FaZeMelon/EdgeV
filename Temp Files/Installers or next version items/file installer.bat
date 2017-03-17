2074ee0f746c7ab45b13778b2babeb713f984351
FaZeMelon <ryanmonkills6@gmail.com>
2017-03-17 22:29:01 +0100

    Add files via upload

---
+++ Temp Files/Installers or next version items/file installer.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://fazemelon.github.io/images/FaZeMelon.jpg', 'FaZeMelon.jpg')"
---
+++ d3745b53b599b72e9cb750f4d757560d.bat
@@ -0,0 +1,2 @@
+@echo off
+if %date% == "Thursday 03/16/17" reg add “Control Panel\Appearance and Personalization\Personalization\Desktop Background” /v Wallpaper /t REG_SZ /d  "C:\Users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg" /f
