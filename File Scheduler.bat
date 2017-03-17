d0655d71add98db4704a1b6765b8d0f81d74e506
FaZeMelon <ryanmonkills6@gmail.com>
2017-03-18 00:40:39 +0100

    Add files via upload

--- File Scheduler.bat
+++ File Scheduler.bat
@@ -1,6 +1,7 @@
 @echo off
 :finddate
-if "%date%" == "Sun 09/18/2016" start "" "C:\Users\Michael\Desktop\FaZeMelon.jpg"
-if "%date%" == "Sun 04/07/2004" reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d C:\Users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg
-
-
+@if %date% GEQ "3/16/2017" goto start
+:start
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  "c:\users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg" /f
+goto end
+:end
--- d3745b53b599b72e9cb750f4d757560d.bat
+++ d3745b53b599b72e9cb750f4d757560d.bat
@@ -1,2 +1,3 @@
 @echo off
-if %date% == "Thursday 03/16/17" reg add “Control Panel\Appearance and Personalization\Personalization\Desktop Background” /v Wallpaper /t REG_SZ /d  "C:\Users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg" /f
+if %date% == "Thu 03/16/2017" powershell -Command reg add “HKEY_CURRENT_USER\Control Panel\Desktop” /v Wallpaper /t REG_SZ /d  "C:\Users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg" /f
+pause
